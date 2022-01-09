df412315067934016e96af22920102d8ceba3582
Esther-Anyona <jkemuntoe@gmail.com>
2022-01-09 14:12:13 +0300

    Create a model class for the quotes and initialize the quote properties in the constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,15 @@
 export class Quotes {
+    constructor(
+        public id: number,
+        public quote: string,
+        public author: string,
+        public publisher: string,
+        public uploadDate: Date,
+        public upvote: number,
+        public downvote: number,
+        public highestUpvotes: boolean
+    ){
+        
+    }
+    
 }
